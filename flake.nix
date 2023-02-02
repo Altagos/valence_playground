2ed2f26e06e0dc0b38f1e8dee1204d4d28e0c3fc
Altagos <altagos@posteo.de>
2023-02-02 14:45:32 +0100

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,38 @@
 {
-  description = "A very basic flake";
+  description = "Valence Playground";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            exa
+            fd
+            cargo-make
+            (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
+          ];
+
+          shellHook = ''
+            alias ls=exa
+            alias find=fd
+          '';
+        };
+      }
+    );
+}
 }
