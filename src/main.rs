58a7c0abf9f2528b3ee2e25e08f97b0bdf44f6ce
Altagos <altagos@posteo.de>
2023-03-03 14:22:55 +0100

    made logging to a file optional

--- src/main.rs
+++ src/main.rs
@@ -16,23 +16,29 @@ use valence_playground::{gui::GuiPlugin, minecraft::MinecraftPlugin};
 pub fn main() {
     dotenv::dotenv().ok();
 
-    let path = std::env::var("RUST_LOG_PATH").expect("RUST_LOG_PATH not defined");
-    let appender = tracing_appender::rolling::never(
-        path,
-        format!("{}.log", Local::now().format("%d.%m.%Y_%H:%M:%S")),
-    );
-    let (non_blocking, _guard) = tracing_appender::non_blocking(appender);
+    if let Ok(path) = std::env::var("RUST_LOG_PATH") {
+        let appender = tracing_appender::rolling::never(
+            path,
+            format!("{}.log", Local::now().format("%d.%m.%Y_%H:%M:%S")),
+        );
+        let (non_blocking, _guard) = tracing_appender::non_blocking(appender);
 
-    tracing_subscriber::registry()
-        .with(EnvFilter::from_default_env())
-        .with(fmt::layer().with_writer(io::stdout))
-        .with(
-            fmt::layer()
-                .with_writer(non_blocking)
-                .compact()
-                .with_ansi(false),
-        )
-        .init();
+        tracing_subscriber::registry()
+            .with(EnvFilter::from_default_env())
+            .with(fmt::layer().with_writer(io::stdout))
+            .with(
+                fmt::layer()
+                    .with_writer(non_blocking)
+                    .compact()
+                    .with_ansi(false),
+            )
+            .init();
+    } else {
+        tracing_subscriber::registry()
+            .with(EnvFilter::from_default_env())
+            .with(fmt::layer().with_writer(io::stdout))
+            .init();
+    }
 
     App::new()
         .add_plugin(MinecraftPlugin)
